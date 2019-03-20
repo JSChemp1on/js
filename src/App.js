d34a119ffe03f4275bd11cd353c99f199648ff65
JSChemp1on <dimitrius.z@gmail.com>
2019-03-20 15:41:52 +0300

    global update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 /*import logo from './logo.svg';*/
 /*import './App.css';*/
-import './app_status.css'
+import './css/app_status.css'
 
 class App extends Component {
   componentDidMount() {

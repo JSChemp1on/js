096142d87d08ee90b41bb701a1fa7fc1b7bc1a27
JSChemp1on <dimitrius.z@gmail.com>
2019-06-25 19:55:44 +0300

    buildbot

---
+++ payment_status_build/precache-manifest.13110bad23510bae8f8650d970221cd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2adf6edae56e1d861c32",
+    "url": "/static/js/main.bc99bf5f.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "07224c901e5a176b52d52e91456e8c30",
+    "url": "/index.html"
+  }
+];
